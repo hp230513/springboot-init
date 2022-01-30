6e3b44782000cd6fc75768ceedbfecbc429d3277
Test <Test@192.168.2.174>
2022-01-30 12:03:44 -0500

    Initial code movement

---
+++ src/main/java/com/td/sbi/SbiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.td.sbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbiDemoApplication.class, args);
+	}
+
+}
